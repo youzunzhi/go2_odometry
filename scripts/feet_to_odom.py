d692f17f621ee1980e7503df9bf61818aed028de
earlaud <etienne.arlaud@inria.fr>
2024-11-07 16:38:40 +0100

    WiP: fix feet odometry script

--- scripts/feet_to_odom.py
+++ scripts/feet_to_odom.py
@@ -14,7 +14,8 @@ class FeetToOdom(Node):
 
     def __init__(self):
         super().__init__('feet_to_odom')
-        self.publisher_ = self.create_publisher(Odometry, 'feet_odom', 10)
+
+        self.publisher_ = self.create_publisher(Odometry, 'odometry/feet', 10)
         self.subscription_ = self.create_subscription(
             LowState,
             '/lowstate',
@@ -28,23 +29,22 @@ class FeetToOdom(Node):
         self.prefilled_msg = Odometry()
         self.prefilled_msg.header.frame_id = "odom"
         self.prefilled_msg.child_frame_id = "utlidar_imu"
-        self.prefilled_msg.pose.covariance = [0]*36
-        self.prefilled_msg.twist.covariance = [0]*36
+        self.prefilled_msg.pose.covariance = [0.]*36
+        self.prefilled_msg.twist.covariance = [0.]*36
 
-        self.prefilled_msg.pose.covariance[2 * (6+1)] = 0.005**2
-        self.prefilled_msg.twist.covariance[7 * (6+1)] = 0.1**2
-        self.prefilled_msg.twist.covariance[8 * (6+1)] = 0.1**2
-        self.prefilled_msg.twist.covariance[9 * (6+1)] = 0.1**2
+        self.prefilled_msg.twist.covariance[0 * (6+1)] = 0.01**2
+        self.prefilled_msg.twist.covariance[1 * (6+1)] = 0.01**2
+        self.prefilled_msg.twist.covariance[2 * (6+1)] = 0.01**2
 
 
     def listener_callback(self, state_msg):
         q = np.array([0]*6 + [1] + [j.q for j in state_msg.motor_state[:12]])
         v = np.array([0]*6 + [j.dq for j in state_msg.motor_state[:12]])
-        self.robot.framesForwardKinematics(q, v)
+        self.robot.forwardKinematics(q, v)
         oMf_list = [self.robot.data.oMf[id] for id in self.foot_frame_id]
         oMi = self.robot.data.oMf[self.imu_frame_id]
-        v_list = [pin.getFrameVelocity(self.model, self.data, id, pin.world) for id in self.foot_frame_id]
-        ovi_o = pin.getFrameVelocity(self.model, self.data, self.imu_frame_id, pin.world)
+        v_list = [pin.getFrameVelocity(self.robot.model, self.robot.data, id, pin.WORLD) for id in self.foot_frame_id]
+        ovi_o = pin.getFrameVelocity(self.robot.model, self.robot.data, self.imu_frame_id, pin.WORLD)
 
         oMf = oMf_list[0]
         ovf_o = v_list[0]
@@ -52,10 +52,11 @@ class FeetToOdom(Node):
         fMi = oMf.actInv(oMi) # TODO : It's not what we want !!
         ovi_f = ovi_o - ovf_o# TODO : It's not what we want !!
         fvi_f = oMf.actInv(ovi_f)
+
+        # Make message
         odom_msg = self.prefilled_msg
-        odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y, odom_msg.pose.pose.position.z = fMi.translation
-        odom_msg.twist.twist.linear.x, odom_msg.twist.twist.linear.y, odom_msg.twist.twist.linear.z = fvi_f.translation
-        odom_msg.twist.twist.angular.x, odom_msg.twist.twist.angular.y, odom_msg.twist.twist.angular.z = fvi_f.rotation
+        odom_msg.header.stamp = self.get_clock().now().to_msg()
+        odom_msg.twist.twist.linear.x, odom_msg.twist.twist.linear.y, odom_msg.twist.twist.linear.z = fvi_f.linear
         self.publisher_.publish(odom_msg)
 
 def main(args=None):
