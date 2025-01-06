3311d6169b87d9ed23521b50d922c19b5ed57dcd
LouiseMsn <louise.manson@inria.fr>
2025-01-06 17:38:29 +0100

    récupération du timestamp du message mocap + fréquence de publication de la tf modifiable

--- scripts/mocap_base_pose.py
+++ scripts/mocap_base_pose.py
@@ -6,6 +6,7 @@ Motion capture : Qualisys with RealTime protocol version 1.24
 
 import asyncio
 import qtm_rt
+from qtm_rt.packet import QRTComponentType
 import xml.etree.ElementTree as ET
 import math
 
@@ -44,7 +45,7 @@ class MocapOdometryNode(Node):
             self.destroy_node() #! to check
             return
         
-        xml_string = await connection.get_parameters(parameters=["6d"])
+        xml_string = await connection.get_parameters(parameters=['6d'])
         self.body_index = self.create_body_index(xml_string)
 
         # disconnect if the wanted body isn't streamed 
@@ -56,74 +57,75 @@ class MocapOdometryNode(Node):
 
     def on_packet(self, packet):
         """ Callback function called every time a data packet arrives from QTM """
-        info, bodies = packet.get_6d()
 
-        i = 0 #index
-        for body in bodies:
-            # select wanted body 
-            if (self.body_index[self.get_parameter("wanted_body").value] == i):
+        # Time management
+        self.new_timestamp = packet.timestamp*0.000001
 
-                position, rotation = body
+        if (self.new_timestamp - self.prec_timestamp) >= (1/self.publishing_freq):
 
-                # Quaternions from rotation matrix
-                qx,qy,qz,qw = self.quaternions_from_rot(rotation)
+            info, bodies = packet.get_6d()
 
-                timestamp = self.get_clock().now().to_msg()
-            
-                # Transform from odom_frame (unmoving) to base_frame (tied to robot base)
-                self.transform_msg.header.stamp = timestamp
-                self.transform_msg.child_frame_id = self.get_parameter("base_frame").value
-                self.transform_msg.header.frame_id = self.get_parameter("odom_frame").value
+            i = 0 #index
+            for body in bodies:
+                # select wanted body 
+                if (self.body_index[self.get_parameter("wanted_body").value] == i):
 
-                # translation + convert to meter
-                self.transform_msg.transform.translation.x = position[0]*0.001
-                self.transform_msg.transform.translation.y = position[1]*0.001
-                self.transform_msg.transform.translation.z = position[2]*0.001
+                    position, rotation = body
 
-                # rotation
-                self.transform_msg.transform.rotation.x = qx
-                self.transform_msg.transform.rotation.y = qy
-                self.transform_msg.transform.rotation.z = qz
-                self.transform_msg.transform.rotation.w = qw
+                    # Quaternions from rotation matrix
+                    qx,qy,qz,qw = self.quaternions_from_rot(rotation)
+
+                    timestamp = self.get_clock().now().to_msg()
+                
+                    # Transform from odom_frame (unmoving) to base_frame (tied to robot base)
+                    self.transform_msg.header.stamp = timestamp
+                    self.transform_msg.child_frame_id = self.get_parameter("base_frame").value
+                    self.transform_msg.header.frame_id = self.get_parameter("odom_frame").value
+
+                    # translation + convert to meter
+                    self.transform_msg.transform.translation.x = position[0]*0.001
+                    self.transform_msg.transform.translation.y = position[1]*0.001
+                    self.transform_msg.transform.translation.z = position[2]*0.001
+
+                    # rotation
+                    self.transform_msg.transform.rotation.x = qx
+                    self.transform_msg.transform.rotation.y = qy
+                    self.transform_msg.transform.rotation.z = qz
+                    self.transform_msg.transform.rotation.w = qw
 
 
-                self.odometry_msg.header.stamp = timestamp
-                self.odometry_msg.child_frame_id = self.get_parameter("base_frame").value
-                self.odometry_msg.header.frame_id = self.get_parameter("odom_frame").value
+                    self.odometry_msg.header.stamp = timestamp
+                    self.odometry_msg.child_frame_id = self.get_parameter("base_frame").value
+                    self.odometry_msg.header.frame_id = self.get_parameter("odom_frame").value
 
-                # position + convert to meter
-                self.odometry_msg.pose.pose.position.x = position[0]*0.001
-                self.odometry_msg.pose.pose.position.y = position[1]*0.001
-                self.odometry_msg.pose.pose.position.z = position[2]*0.001 
+                    # position + convert to meter
+                    self.odometry_msg.pose.pose.position.x = position[0]*0.001
+                    self.odometry_msg.pose.pose.position.y = position[1]*0.001
+                    self.odometry_msg.pose.pose.position.z = position[2]*0.001 
 
-                #orientation
-                self.odometry_msg.pose.pose.orientation.x = qx
-                self.odometry_msg.pose.pose.orientation.y = qy
-                self.odometry_msg.pose.pose.orientation.z = qz
-                self.odometry_msg.pose.pose.orientation.w = qw 
+                    #orientation
+                    self.odometry_msg.pose.pose.orientation.x = qx
+                    self.odometry_msg.pose.pose.orientation.y = qy
+                    self.odometry_msg.pose.pose.orientation.z = qz
+                    self.odometry_msg.pose.pose.orientation.w = qw 
 
-                # linear speed
-                self.odometry_msg.twist.twist.linear.x = 0. 
-                self.odometry_msg.twist.twist.linear.y = 0.
-                self.odometry_msg.twist.twist.linear.z = 0.
+                    # linear speed
+                    self.odometry_msg.twist.twist.linear.x = 0. 
+                    self.odometry_msg.twist.twist.linear.y = 0.
+                    self.odometry_msg.twist.twist.linear.z = 0.
 
-                # angular speed
-                self.odometry_msg.twist.twist.angular.x = 0.
-                self.odometry_msg.twist.twist.angular.y = 0.
-                self.odometry_msg.twist.twist.angular.z = 0.
+                    # angular speed
+                    self.odometry_msg.twist.twist.angular.x = 0.
+                    self.odometry_msg.twist.twist.angular.y = 0.
+                    self.odometry_msg.twist.twist.angular.z = 0.
+                    self.tf_broadcaster.sendTransform(self.transform_msg)
+                    self.odometry_publisher.publish(self.odometry_msg)
 
-            else:
-                pass # ignore other bodies
+                else:
+                    pass # ignore other bodies
 
-            i += 1
-
-        # Publishing only one for two messages to reduce frequency to approx 150hz
-        if self.j == self.decimation_count: 
-            self.tf_broadcaster.sendTransform(self.transform_msg)
-            self.odometry_publisher.publish(self.odometry_msg)
-            self.j = 0
-        else:
-            self.j += 1
+                i += 1
+            self.prec_timestamp = self.new_timestamp
 
                 
     def create_body_index(self,xml_string):
@@ -178,8 +180,9 @@ class MocapOdometryNode(Node):
     transform_msg = TransformStamped()
     odometry_msg = Odometry()
     body_index = {}
-    j = 0 # Decimation counter for reducing mocap messages freq
-    decimation_count = 1 # Decimation count 
+    publishing_freq = 110 # in Hz : due to the discretisation the frequency may be slightly lower than what is it set to. Max limit of 300Hz (set in MoCap software)
+    new_timestamp = 0
+    prec_timestamp = 0
 
 
 def main(args=None):
