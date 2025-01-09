daab3e4d1528394abfd39d072da6ad856ff8789d
LouiseMsn <louise.manson@inria.fr>
2025-01-09 17:22:47 +0100

    correction of freq as ros param

--- scripts/mocap_base_pose.py
+++ scripts/mocap_base_pose.py
@@ -85,9 +85,9 @@ class MocapOdometryNode(Node):
                     self.transform_msg.header.frame_id = self.get_parameter("odom_frame").value
 
                     # translation + convert to meter
-                    self.transform_msg.transform.translation.x = position[0]*0.001
-                    self.transform_msg.transform.translation.y = position[1]*0.001
-                    self.transform_msg.transform.translation.z = position[2]*0.001
+                    self.transform_msg.transform.translation.x = position[0] #*0.001
+                    self.transform_msg.transform.translation.y = position[1] #*0.001
+                    self.transform_msg.transform.translation.z = position[2] #*0.001
 
                     # rotation
                     self.transform_msg.transform.rotation.x = qx
