970057bb3e955f0f8f967a0c83b3e71fc521de1d
LouiseMsn <louise.manson@inria.fr>
2025-03-11 16:25:05 +0100

    forwarded launch parameters from node script to go2_odometry_switch.launch.py

--- launch/go2_odometry_switch.launch.py
+++ launch/go2_odometry_switch.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, PythonExpression
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, PythonExpression, TextSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
 
@@ -13,6 +13,34 @@ def generate_launch_description():
         description='Type of odometry desired between : fake, mocap'
     )
 
+    mocap_base_frame_arg = DeclareLaunchArgument(
+                            'base_frame',
+                            default_value='base',
+                            description='[IF MOCAP] Name of the base frame (fixed to robot) of the mocap.'
+    )
+    mocap_odom_frame_arg = DeclareLaunchArgument(
+                            'odom_frame',
+                            default_value='odom',
+                            description='[IF MOCAP] Name of the odom frame (fixed to world) of the mocap.'
+    )
+    mocap_object_name_arg = DeclareLaunchArgument(
+                            'wanted_body',
+                            default_value='Go2',
+                            description='[IF MOCAP] Name of the object tracked by the motion capture.'
+    )
+    mocap_ip_arg = DeclareLaunchArgument(
+                    'qualisys_ip',
+                    default_value="192.168.75.2",
+                    description='[IF MOCAP] IP used by the motion capture to publish data.'
+    )
+    mocap_publishing_freq_arg = DeclareLaunchArgument(
+                                'publishing_freq',
+                                default_value=TextSubstitution(text='110'),
+                                description='[IF MOCAP] Publishing frequency of the odom to robot base transform. Max limit of 300Hz.'
+    )
+
+
+# launchfiles
     mocap_launch_file = PathJoinSubstitution([
                         FindPackageShare('go2_odometry'),
                         'launch',
@@ -32,20 +60,28 @@ def generate_launch_description():
                                   ])
 
     return LaunchDescription([
+        mocap_base_frame_arg,
+        mocap_odom_frame_arg,
+        mocap_object_name_arg,
+        mocap_ip_arg,
+        mocap_publishing_freq_arg,
 
         odom_type_arg,
 
-        IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([mocap_launch_file]),
-        condition=IfCondition(PythonExpression(["'",LaunchConfiguration('odom_type'),"' == 'mocap'"]))
+        IncludeLaunchDescription
+        (
+            PythonLaunchDescriptionSource([mocap_launch_file]),
+            condition=IfCondition(PythonExpression(["'",LaunchConfiguration('odom_type'),"' == 'mocap'"]))
         ),
 
-        IncludeLaunchDescription(
+        IncludeLaunchDescription
+        (
             PythonLaunchDescriptionSource([fake_odom_launch_file]),
             condition=IfCondition(PythonExpression(["'",LaunchConfiguration('odom_type'),"' == 'fake'"]))
         ),
 
-        IncludeLaunchDescription(
+        IncludeLaunchDescription
+        (
             PythonLaunchDescriptionSource([full_state_publisher_launch_file]),
             condition=IfCondition(PythonExpression(["'",LaunchConfiguration('odom_type'),"' == 'use_full_odom'"]))
         )
